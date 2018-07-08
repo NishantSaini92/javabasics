55bc5b65fc45a85c371bf9445870533fdf589346
NishantSaini <nishantsainins@gmail.com>
2018-07-08 12:01:57 -0400

    simpleclass

---
+++ src/collectionofprogram/SimpleClass.java
@@ -0,0 +1,11 @@
+package collectionofprogram;
+
+public class SimpleClass {
+//class is keyword to define class
+	//SimpleClass is the name of class
+	// variables,methods,constructors
+	public static void main(String[] args) {
+		System.out.println("class");
+	}
+
+}
